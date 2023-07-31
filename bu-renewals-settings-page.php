c2c5d88b2b21a2df5181db7640c2b65193dc5bea
isabell austin <iaustin@butler.edu>
2023-07-31 13:04:09 -0400

    adding other files

---
+++ bu-renewals-settings-page.php
@@ -0,0 +1,289 @@
+<?php
+
+class bu_renewals_optionspage {
+  var $group;
+  var $page;
+  var $options;
+
+  var $title;
+  var $buNetworkSaveError = '';
+
+  /**
+	 * bu_renewals_optionspage(0)
+	 *
+	 * Rendering option page.
+	 *
+	 * @param [type] $group
+	 * @param [type] $page
+	 * @param [type] $options
+	 * @return void
+	 */
+  function __construct( $group, $page, $options ) {
+    $this -> group = $group;
+    $this -> page = $page;
+    $this -> options = $options;
+    $this -> title = "Site Renewals";
+
+    if ( $_POST && isset($_POST[$group]) ) {
+      $this -> saveNetworkOptions($group);
+    }
+
+    if ( isset($_REQUEST['bu-renewal-reset']) && ($_REQUEST['bu-renewal-reset'] == 'YES') ) {
+      global $wpdb;
+      $wpdb->get_results( "DELETE FROM $wpdb->sitemeta WHERE meta_key LIKE 'bu_%_renewed'" );
+      $this -> buNetworkSaveError = "<div id='message' class='updated fade'><p>All renewal data has been deleted.</p></div>\n";
+    }
+
+    $this -> options = get_network_option( 1, $group, array() );
+
+    add_action( 'admin_init', array(&$this, 'register_options') );
+    add_action( 'network_admin_menu', array(&$this, 'add_networksettings_page') );
+
+  }
+
+
+	/**
+	 * saveNetworkOptions()
+	 *
+	 * Group will normally be bu_renewals_options_ms, but could possibly change in the future
+	 *
+	 * @param [type] $group
+	 * @return void
+	 */
+  function saveNetworkOptions($group) {
+
+    $settings = array();
+
+    foreach ( array('enabled','end_date','bu_renewal_text') as $field ) {
+      $settings[$field] = isset( $_POST[$group][$field] ) ? $_POST[$group][$field] : '';
+    }
+    update_network_option( 1, $group, $settings );
+
+    $this -> buNetworkSaveError = "<div id='message' class='updated fade'><p>Settings updated.</p></div>\n";
+
+  }
+
+	/**
+	 * register_options()
+	 *
+	 * Register the options for this plugin so they can be displayed and updated below.
+	 *
+	 * @return void
+	 */
+  function register_options() {
+
+    register_setting( $this -> group, $this -> group );
+
+    #-- Create the 'bu_renewals_main' section and add fields to it
+    $section = 'bu_renewals_main';
+    add_settings_section( $section, 'General Settings', array(&$this, '_display_options_section'), $this -> page );
+		add_settings_field( 'bu_renewals_enabled', 'Renewals Enabled?', array(&$this, '_display_option_enabled'), $this -> page, $section );
+		add_settings_field( 'bu_renewals_renwal_text', '<label>Renewal Text Prompt:</label>', array(&$this, '_display_option_renewaltext'), $this -> page, $section );
+    add_settings_field( 'bu_renewals_end_date', '<label>Renewal End Date:</label>', array(&$this, '_display_option_end_date'), $this -> page, $section );
+		add_settings_field( 'bu_renewals_reset_button', 'Reset Renewal Data:', array(&$this, '_display_option_reset_button'), $this -> page, $section );
+
+  }
+
+/*
+  function _display_date_picker($label) {
+  	print "<label for=$label>End Date:</label>"
+  	print "<input type="date">"
+  }
+ */
+	/**
+	 * _display_options_section
+	 *
+	 * Unused - Display explanatory text for the main options section.
+	 *
+	 * @return void
+	 */
+  function _display_options_section() {
+  }
+
+	/**
+	 * _display_radio_buttons
+	 *
+	 * Display radio buttons
+	 *
+	 * @param [type] $groupID
+	 * @param [type] $value
+	 * @param [type] $radio_options
+	 * @param integer $noNewlines
+	 * @return void
+	 */
+  function _display_radio_buttons( $groupID, $value, $radio_options, $noNewlines = 0 ) {
+
+    print "\n<p>\n";
+    foreach( $radio_options as $option => $name ) {
+      $fid = htmlspecialchars( $this -> group ) . '_' . htmlspecialchars( $groupID ) . '_' . $option;
+      $fname = htmlspecialchars( $this -> group ) . '[' . htmlspecialchars( $groupID ) . ']';
+
+      print '<input type="radio" name="' . $fname . '" id="' . $fid . '"' . ( $value === $option ? ' checked="checked"' : '' ) . ' value="' . $option . '"/><label for="' . $fid . '">' . $name  . '</label>' . ( !$noNewlines ? "<br />\n" : '  ' );
+    }
+    print "</p>\n";
+
+  }
+
+	/**
+	 * _display_checkbox
+	 *
+	 * Display checkbox
+	 *
+	 * @param [type] $groupID
+	 * @param [type] $value
+	 * @param [type] $label
+	 * @return void
+	 */
+  function _display_checkbox( $groupID, $value, $label ) {
+
+    $fid = htmlspecialchars( $this -> group ) . '_' . htmlspecialchars( $groupID );
+    $fname = htmlspecialchars( $this -> group ) . '[' . htmlspecialchars( $groupID ) . ']';
+
+    print "\n<p><input type='checkbox' name='" . $fname . "' id='" . $fid . "'" . ( $value ? " checked='checked'" : '' ) . "/><label for='" . $fid . "'>" . $label  . "</label></p>\n";
+
+  }
+
+	/**
+	 * _display_textarea
+	 *
+	 * Display textarea
+	 *
+	 * @param [type] $groupID
+	 * @param [type] $value
+	 * @param integer $mode
+	 * @return void
+	 */
+  function _display_textarea( $groupID, $value, $mode = 0 ) {
+
+    $fid = htmlspecialchars( $this -> group ) . '_' . htmlspecialchars( $groupID );
+    $fname = htmlspecialchars( $this -> group ) . '[' . htmlspecialchars( $groupID ) . ']';
+
+    print "\n<p><textarea type='textarea' name='" . $fname . "' id='" . $fid . "rows='7' cols='50'>" . htmlspecialchars( $value ) . "</textarea></p>\n";
+  }
+
+	/**
+	 * _display_datepicker
+	 *
+	 *  Display textfield
+	 *
+	 * @param [type] $groupID
+	 * @param [type] $value
+	 * @param [type] $size
+	 * @param integer $mode
+	 * @return void
+	 */
+  function _display_datepicker( $groupID, $value, $size, $mode = 0 ) {
+
+    $fid = htmlspecialchars( $this -> group ) . '_' . htmlspecialchars( $groupID );
+    $fname = htmlspecialchars( $this -> group ) . '[' . htmlspecialchars( $groupID ) . ']';
+
+    print "\n<p><input type='date' name='" . $fname . "' id='" . $fid . "' size='" . $size . "' maxlenbuh='" . $size . "' value='" . htmlspecialchars( $value ) . "'/></p>\n";
+  }
+
+	/**
+	 * _display_option_enabled
+	 *
+	 * Display the privacy radio buttons
+	 *
+	 * @return void
+	 */
+  function _display_option_enabled() {
+
+    $currentValue = $this -> options['enabled'];
+    $this -> _display_radio_buttons( 'enabled', $currentValue, array('N' => 'Renewals are disabled', 'Y' => 'Renewals are enabled') );
+
+    print "<p class='description'>Disable the system to hide all messages from all users.  When enabling, for a new round of renewals, don't forget to reset the renewal data.</p>\n";
+
+  }
+
+	/**
+	 * _display_option_renewaltext
+	 *
+	 * Display the visible renewal text
+	 *
+	 * @return void
+	 */
+  function _display_option_renewaltext() {
+
+    $currentValue = $this -> options['bu_renewal_text'];
+    $this -> _display_textarea( 'bu_renewal_text', $currentValue );
+		print "<p class='description'>Provide boilerplate text for the renewal prompts.</p>";
+  }
+
+	/**
+	 * _display_option_end_date
+	 *
+	 * Display the visible end date for renewals Field
+	 *
+	 * @return void
+	 */
+  function _display_option_end_date() {
+    $end_date = $this -> options['end_date'];
+    $this -> _display_datepicker( 'end_date', $end_date, 60 );
+	
+
+    //INCREMENTING THE RENEWAL DATE
+    // $annual = strtotime($end_date . ' + 1 Year');
+    // echo date('m/d/Y', $annual);
+  }
+
+	/**
+	 * _display_option_reset_button
+	 *
+	 * Throw caution into the wind, and destroy all things.
+	 *
+	 * @return void
+	 */
+  function _display_option_reset_button() {
+        print "<p><input class='button-primary' type='button' value='Reset Renewal Data' onClick=\"\n";
+        print "if (confirm('Are you sure you want to reset all renewal data? (This operation cannot be undone!)')) {\n";
+        print "document.location='?page=bu-renewals-config&amp;bu-renewal-reset=YES';\n";
+        print "}\"></p>\n";
+  }
+
+	#---------- END OPTIONS DEFINITIONS ----------#
+
+  ## Add a network options page for this plugin.
+  function add_networksettings_page() {
+    add_submenu_page( 'settings.php', $this -> title, $this -> title, 'manage_options', $this -> page, array(&$this, '_display_networksettings_page') );
+  }
+
+  ## Display the options for this plugin.
+  function _display_networksettings_page() {
+
+    $this -> _display_settings_page();
+
+  }
+
+  function _display_settings_page() {
+
+    if ( !current_user_can('manage_options') ) {
+      wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+
+    if ( $this -> buNetworkSaveError != '' ) {
+      print $this -> buNetworkSaveError;
+    }
+
+    print "<div class='wrap'>\n";
+    print "<h1>Site Renewal Settings</h1>\n";
+    print "<form action='' method='post'>\n";
+
+    #-- Internal WP Fields
+    settings_fields( $this -> group );
+    print "\n";
+
+    #-- The actual options settings that users can see and change
+    do_settings_sections( $this -> page );
+    print "\n";
+
+    print "<p class='submit'>\n";
+    print "<input type='submit' name='Submit' class='button-primary' value=\"";
+    esc_attr_e( 'Save Changes' );
+    print "\" />\n";
+    print "</p>\n";
+    print "</form>\n";
+    print "</div>\n";
+  }
+}
+?>
---
+++ bu-renewals.php
@@ -0,0 +1,374 @@
+<?php
+
+/*
+Plugin Name: BU Site Renewal Service
+Version: 1.2
+Description: Provides mechanism for having site owners renew their sites on a periodic basis.
+Author: Professional Web Presence
+Network: true
+*/
+
+require_once( __DIR__ . DIRECTORY_SEPARATOR . 'bu-renewals-settings-page.php' );
+require_once( __DIR__ . DIRECTORY_SEPARATOR . 'bu-renewals-dashboard.php' );
+
+/**
+ * bu_renewals_add_dashboard_widget()
+ *
+ * Current, site-specific dashboard.
+ *
+ * @return void
+ */
+function bu_renewals_add_dashboard_widget() { // for each blog in blogs; USED IN bu_renewals_init()
+	$blogID = get_current_blog_id();
+	bu_renewals_traffic_cop($blogID, "widget");
+}
+
+/**
+ * bu_renewals_content()
+ *
+ * The meat of any renewal request.
+ *
+ * @return void
+ */
+function bu_renewals_content( $blogID ) { // SITE DASHBOARD RENEWAL CONFIRM/DENY VIEW FOR ADMINS/DETERMINED USERS; SC#3
+
+	$option = 'bu_' . $blogID . '_site_renewed';
+	$renewed = get_site_option( $option, '' );
+	$currentDate = strtotime( date('F d, Y') );
+	
+	if ( $renewed ) {
+		bu_renewals_content_confirmed($blogID);
+
+	}  
+	else {
+		$options = get_network_option( 1, 'bu_renewals_config', array('enabled' => 'N', 'end_date' => '') );
+		$selectedDate = date_create( $options['end_date'] );
+		$end_date = date_format( $selectedDate,"F d, Y" );
+		$renewal = strtotime( $end_date );
+
+		if ( $renewal < $currentDate ){ //Checks if the renewal date has passed
+			print "<p>The renewal deadline of <strong>" . $end_date. "</strong> has passed. Your site will be archived for 9 months and then deleted. Please contact the Butler IT Help Desk for retrieval or any other inquires.</p>\n";
+		}
+		else {
+			if ( $options['bu_renewal_text'] ){
+				print "<p>" . $options['bu_renewal_text'] . "</p>";
+			}
+
+			print "<p>You have until <strong>" . $end_date. "</strong> to renew the website <i>'" . get_bloginfo('name') . "'</i>. Non-renewed websites will be archived for 9 months and then deleted.</p>\n";
+			print "<p class=\"bu-button-blue\" style=\"text-align: center;\">\n";
+			print "  <a href=\"?bu-renew-site=Y\">Renew " . get_bloginfo('name') . "</a>\n";
+			print "  <a href=\"ms-delete-site.php\">Delete " . get_bloginfo('name') . "</a>\n";
+		print "</p>\n";
+		}
+	}
+}
+
+/**
+ * bu_renewals_content_nonadmin()
+ *
+ * Renewal requests for non-admins.
+ *
+ * @return void
+ */
+function bu_renewals_content_nonadmin( $blogID ) { // SITE DASHBOARD RENEWAL CONFIRM/DENY VIEW FOR NON-ADMINS
+
+	$option = 'bu_' . $blogID . '_site_renewed';
+	$renewed = get_site_option( $option, '' );
+
+	if ( $renewed ) {
+		bu_renewals_content_confirmed( $blogID );
+	}
+
+	else {
+		$options = get_network_option( 1, 'bu_renewals_config', array('enabled' => 'N', 'end_date' => '') );
+
+		if ( $options['bu_renewal_text'] ){
+			print "<p>" . $options['bu_renewal_text'] . "</p>";
+		}
+
+		$selectedDate = date_create( $options['end_date'] );
+		$end_date = date_format( $selectedDate,"F d, Y" );
+
+		print "<p>Renewal of this website, <i>'" . get_bloginfo( 'name' ) . "'</i>, is due on <strong>" . $options['end_date'] . "</strong>. Non-renewed websites will be archived and deleted.</p>\n";
+		print "<h4>Site Administators</h4>\n";
+		print "<p>Please contact one of the website administrators listed below to complete the renewal process.</p>\n";
+
+		$blogID = get_current_blog_id();
+		$userAdmin = get_users( array('blog_id'=> $blogID, 'role' => 'administrator') );
+		if ( $userAdmin ){
+			foreach( $userAdmin as $user ){
+				print "<p><a href=mailto:'" . $user->user_email . "'>" . $user->display_name . "</a></p>\n";
+				// $to = $user->user_email;
+				// $subject = "It is time to renew this WP website! ";
+				// $message = "You have until " . $end_date. " to renew the website " . get_bloginfo('name') . ". Non-renewed websites will be archived and deleted.";
+				// $headers = get_bloginfo('name');
+				// wp_mail( $to, $subject, $message, $headers );
+			}
+		}
+	  print "\n";
+	}
+}
+
+/**
+ * bu_renewals_content_confirmed()
+ *
+ * Confirmed messsage.
+ *
+ * @return void
+ */
+function bu_renewals_content_confirmed( $blogID ) { // USED IN bu_renewals_content; POST-RENEWAL, CONFIRMATION DASHBOARD WIDGET; SC#4
+
+	if ( $blogID == "" ){
+		// why do i have to do this? WordPress widgets are a mysterious beast.
+		$blogID = get_current_blog_id();
+	}
+
+	global $wpdb;
+
+	$option = 'bu_' . $blogID . '_site_renewed';
+	$renewed = get_site_option( $option, '' );
+
+	if ( $renewal = $wpdb->get_results( "SELECT meta_value FROM wp_sitemeta WHERE meta_key LIKE 'bu_". $blogID . "_site_renewed'" ) ) {
+		$details = explode( '|', $renewal[0] -> meta_value );
+
+		if ( $user = $wpdb->get_results( "SELECT display_name FROM wp_users WHERE user_login LIKE '" . $details[2] . "'" ) ) {
+      $userName = $user[0] -> display_name;
+		}
+
+		$rawDate = strtotime( $details[1] );
+
+		print "<p style='font-size: 120%; font-weight: 800;'>Thank you for renewing your website!</p>\n";
+    print "<p style='font-size: 100%;'>  Your website was renewed on <strong>" .  date( 'M j, Y', $rawDate ) . "</strong> by <strong>" . ( isset($userName) ? $userName : $details[2] ) . "</strong>.</p>";
+
+	}
+	else {
+		print "<p class='notice notice-error'>Error: Unable to find website renewal details!</p>\n";
+	}
+}
+
+function  bu_renewals_deactivate_blog( $id ) {
+	$id = get_current_blog_id();
+	echo 'dsafeafshertrytfewrsfdghrtserghhhjhvjhjkhhjhjhjkb jbnmjbnmj' . $id;
+}
+
+/**
+ * bu_renewals_traffic_cop
+ *
+ * Redirects renewal traffic to admin, non-admin views.
+ *
+ * @param [type] $blogID
+ * @return void
+ */
+function bu_renewals_traffic_cop( $blogID, $type ){ // USED IN bu_renewals_add_dashboard_widget()
+	// if blogID=1, get out.
+	if ( $blogID == 1 ){
+		return;
+	}
+
+	$flag = 0;
+	$currentUser = wp_get_current_user();
+
+	// method derived from https://wordpress.stackexchange.com/a/131816
+	$userClean = get_users( array('blog_id'=> $blogID, 'include' => array($currentUser->ID)) );
+	$user = $userClean[0];
+	$allowed_roles = array( 'administrator' );
+
+	$option = 'bu_' . $blogID . '_site_renewed';
+	$renewed = get_site_option( $option, '' );
+
+	// if admin exists
+	if ( array_intersect($allowed_roles, $user->roles) ) {
+		if ( $type == "widget" ){
+			$flag = 1;
+
+			$options = get_network_option( 1, 'bu_renewals_config', array('enabled' => 'N', 'end_date' => '') );
+			$selectedDate = date_create( $options['end_date'] );
+			$end_date = date_format( $selectedDate,"F d, Y" );
+			$renewal = strtotime( $end_date );
+			$currentDate = strtotime( date('F d, Y') );
+
+			if ( $renewal < $currentDate ) {
+				// If renewal date has passed
+				wp_add_dashboard_widget( 'bu_renewals', "Renewal date for '" . get_bloginfo('name') . "' has passed.", 'bu_renewals_content', '', $blogID );
+				bu_renewals_deactivate_blog($blogID);
+				add_action( 'deactivate_blog', 'bu_renewals_deactivate_blog' );
+				// do_action( 'deactivate_blog', $id );
+
+			}
+			else if ( !$renewed && !isset($_REQUEST['bu-renew-site']) ) {
+				// Renewal widget.
+				wp_add_dashboard_widget( 'bu_renewals', "Please Renew '" . get_bloginfo('name') . "'", 'bu_renewals_content', '', $blogID );
+			}
+
+			else if ( ($renewed == '') && isset($_REQUEST['bu-renew-site']) && ($_REQUEST['bu-renew-site'] == 'Y') ) {
+				// For when renewal is first submitted - save and display a thank you message
+				$user = wp_get_current_user();
+				update_site_option( $option, $blogID . '|' . date('Y-m-d H:i:s') . "|" . $user -> user_login );
+				wp_add_dashboard_widget( 'bu_renewals', 'Website Renewal Confirmed', 'bu_renewals_content_confirmed', '', $blogID );
+			}
+
+			else if ( $renewed ) {
+				// If renewal was previously submitted, show that it's already in the system
+				wp_add_dashboard_widget( 'bu_renewals', 'Website Renewal Confirmed', 'bu_renewals_content_confirmed' );
+			}
+			else {
+
+			}
+		}
+		else if ( $type == "sites" ) {
+
+			if ( $renewed ) {
+				// If renewal was previously submitted, show that it's already in the system
+				bu_renewals_confirm_my_sites();
+			}
+			else {
+				bu_renewals_deny_my_sites();
+			}
+		}
+
+		else {
+
+		}
+	}
+
+	// non-admin operation
+	else {
+
+		if ( $type == "widget" ){
+			wp_add_dashboard_widget( 'bu_renewals', "Renewal Required for '" . get_bloginfo('name') . "'", 'bu_renewals_content_nonadmin', '', $blogID );
+			$flag = 1;
+		}
+		elseif ( $type == "sites" ) {
+			if ( $renewed ) {
+				// If renewal was previously submitted, show that it's already in the system
+				bu_renewals_confirm_my_sites();
+			}
+			else {
+				bu_renewals_deny_my_sites();
+			}
+		}
+		$flag = 1;
+	}
+
+	if( $type == "widget") {
+		if ( $flag ) {
+
+			// Globalize the metaboxes array, this holds all the widgets for wp-admin
+			global $wp_meta_boxes;
+
+			// Get the regular dashboard widgets array
+			// (which has our new widget already but at the end)
+
+			$normal_dashboard = $wp_meta_boxes['dashboard']['normal']['core'];
+
+			// Backup and delete our new dashboard widget from the end of the array
+
+			$example_widget_backup = array( 'bu_renewals' => $normal_dashboard['bu_renewals'] );
+			unset( $normal_dashboard['bu_renewals'] );
+
+			// Merge the two arrays together so our widget is at the beginning
+
+			$sorted_dashboard = array_merge( $example_widget_backup, $normal_dashboard );
+
+			// Save the sorted array back into the original metaboxes
+
+			$wp_meta_boxes['dashboard']['normal']['core'] = $sorted_dashboard;
+		}
+	}
+}
+
+function bu_renewals_confirm_my_sites(){
+	print "<p class='notice notice-success'>Website renewal confirmed!</p>\n";
+
+}
+
+function bu_renewals_deny_my_sites(){
+	print "<p class='notice notice-error'>Website has not been renewed.</p>\n";
+	// $blogID = get_current_blog_id();
+	// do_action( 'archive_blog', $blogID );
+}
+
+/**
+ * function bu_renewals_widget_my_sites
+ *
+ * Processes a renewal form on My Sites
+ *
+ * @param [type] $actions
+ * @param [type] $user_blog
+ * @return void
+ */
+function bu_renewals_widget_my_sites( $actions, $user_blog ) { // USED IN bu_renewals_init()
+	$blogID = $user_blog->userblog_id;
+	bu_renewals_traffic_cop( $blogID, "sites" );
+	return $actions;
+}
+
+/**
+ * function bu_renewals_new_site
+ *
+ * Processes the renewal for a site created while active.
+ *
+ * @param [type] $blog_id
+ * @param [type] $user_id
+ * @param [type] $domain
+ * @param [type] $path
+ * @param [type] $site_id
+ * @param [type] $meta
+ * @return void
+ */
+function bu_renewals_new_site ( $blog_id, $user_id, $domain, $path, $site_id, $meta ) { // USED IN bu_renewals_init()
+
+	// getting blog details from arguments + calls
+	$blogID = $blog_id;
+	$option = 'bu_' . $blogID . '_site_renewed';
+	$user = wp_get_current_user();
+
+	//renewal function //
+	update_site_option( $option, $blogID . '|' . date('Y-m-d H:i:s') . "|" . $user->user_login );
+
+}
+
+
+/**
+ * bu_renewals_css_enqueue
+ *
+ * Queue in the CSS, Batman!
+ *
+ * @return void
+ */
+function bu_renewals_css_enqueue() { // USED IN bu_renewals_init()
+  wp_enqueue_style( 'bu_renewals_css', plugins_url('bu-renewals.css', __FILE__) );
+}
+
+/**
+ * bu_renewals_init()
+ *
+ * Init function.
+ *
+ * @return void
+ */
+function bu_renewals_init() {
+  $options = get_network_option( 1, 'bu_renewals_config', array('enabled' => 'N', 'end_date' => '') );
+
+  new bu_renewals_optionspage( 'bu_renewals_config', 'bu-renewals-config', $options );
+
+  add_action( 'admin_enqueue_scripts', 'bu_renewals_css_enqueue' );
+
+  if ( $options['enabled'] == 'Y' ) {
+		add_action( 'wp_dashboard_setup', 'bu_renewals_add_dashboard_widget' );
+		add_filter( 'myblogs_blog_actions', 'bu_renewals_widget_my_sites', 10, 2 );
+  }
+
+	add_action( 'wp_network_dashboard_setup', 'bu_renewals_add_network_dashboard_widget' );
+
+	// conditional on whether new site config is called.
+	$mu_options = get_network_option( 1, 'bu_renewals_config', array('enabled' => 'N', 'end_date' => '') );
+
+	if ( $mu_options['enabled'] == 'Y' && date("Y-m-d H:i:s",strtotime($mu_options['end_date'])) >= date("Y-m-d H:i:s") ) {
+		add_action( 'wpmu_new_blog', 'bu_renewals_new_site' );
+	}
+}
+
+bu_renewals_init();
+
+?>
